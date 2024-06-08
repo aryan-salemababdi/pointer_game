5a32a9fb426cc188695c13d02bad88f4057de3fb
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2024-06-08 04:03:53 +0330

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { Box } from "@mui/material";
 import Landing from './components/molecule/Landing/Landing'
 import GamePage from './components/molecule/GamePage/GamePage';
 import ResultPage from './components/molecule/ResultPage/ResultPage';
+
 
 function App() {
   const [gameMode, setGameMode] = useState<string>("");
@@ -9,33 +11,33 @@ function App() {
 
   return (
     <>
-    <Box sx={{m:0 ,p:0, overflow:"hidden"}}>
-      {
-        gameMode === "" ? (
-          <Landing
-            selected={(select) => {
-              setGameMode(select)
-            }}
-          />
-        ) : resultPage ? (
-          <ResultPage
-            setMenuPage={(status, menu) => {
-              setGameMode(status)
-              setResultPage(menu)
-            }}
-          />
-        ) : (
-          <GamePage
-            role={gameMode}
-            setBackPage={(state) => {
-              setGameMode(state)
-            }}
-            result={(resultPage) => {
-              setResultPage(resultPage);
-            }}
-          />
-        )
-      }
+      <Box sx={{ m: 0, p: 0, overflow: "hidden" }}>
+        {
+          gameMode === "" ? (
+            <Landing
+              selected={(select) => {
+                setGameMode(select)
+              }}
+            />
+          ) : resultPage ? (
+            <ResultPage
+              setMenuPage={(status, menu) => {
+                setGameMode(status)
+                setResultPage(menu)
+              }}
+            />
+          ) : (
+            <GamePage
+              role={gameMode}
+              setBackPage={(state) => {
+                setGameMode(state)
+              }}
+              result={(resultPage) => {
+                setResultPage(resultPage);
+              }}
+            />
+          )
+        }
       </Box>
     </>
   )
